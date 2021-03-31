4910cffb546f92dcd7925e577f01a2383ad7b860
warriorwithing-code <sephirotwwh666@hotmail.com>
2021-03-31 16:21:44 +0200

    add entity framework

---
+++ Aplication/Dto/GameDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Dto
+{
+    public class GameDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int GenderId { get; set; }
+        public string Info { get; set; }
+    }
+}
---
+++ BBDD/Repositories/GamesRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBDD.Repositories
+{
+    class GamesRepository
+    {
+        public void add_Game()
+        {
+
+        }
+
+        public void load_Game()
+        {
+
+        }
+
+        public void load_Games()
+        {
+
+        }
+    }
+}
